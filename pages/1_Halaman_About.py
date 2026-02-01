3f0e5d4df0f70e0c394d2ff7fb679f38524a270c
FAHMI-RIDANI <babangrdn11@gmail.com>
2026-02-01 16:58:12 +0700

    folder page

---
+++ pages/1_Halaman_About.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Analisis Pola Pembelian Butik Emas", page_icon="📊", layout="wide"
+)
+
+st.title("Aplikasi Analisis Pola Pembelian Pelanggan")
+st.subheader("(FP-Growth)")
+
+st.write(
+    """
+    Aplikasi ini digunakan untuk menganalisis pola pembelian pelanggan
+    berdasarkan data transaksi penjualan.  
+    """
+)
+
+st.info("Silakan pilih menu halaman app pada sidebar untuk memulai.")
+
+st.write(
+    """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar 
+libero lectus, a auctor libero lacinia nec. In hac habitasse platea dictumst.
+Fusce ac fermentum arcu, et lacinia augue. Pellentesque interdum dolor ut tempor finibus. 
+Pellentesque vulputate eget leo non consectetur. Sed in efficitur ex. Duis hendrerit, 
+erat nec cursus interdum, sem urna ultricies felis, sed rutrum leo enim at ligula. 
+Etiam convallis, erat eu mollis dapibus, turpis nisl tempus purus, sit amet laoreet ipsum arcu at dui.
+Donec fringilla purus et nulla tempor, quis tincidunt purus placerat. Suspendisse nec elit 
+ut tortor imperdiet bibendum sed in massa. Sed faucibus.
+    """
+)
